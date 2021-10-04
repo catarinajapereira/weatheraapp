ec76ab91aebc0e8519ee727ac50f96a8af93736f
catarinajapereira <87657577+catarinajapereira@users.noreply.github.com>
2021-10-04 14:11:50 +0100

    design changes

--- index.js
+++ index.js
@@ -50,6 +50,8 @@ formcity.addEventListener("submit", search);
 //TRUE TEMP_________________________________________
 
 function displayWeatherCondition(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#Citytemp").innerHTML = response.data.name;
 
   celsiustemperature = response.data.main.temp;
@@ -164,3 +166,4 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
+searchcity("Lisbon");
